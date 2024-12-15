df23db9202d5aed4763a5fa7b1d6ee2985ba048e
ahmedwael <ahmedwael99104@gmail.com>
2024-12-15 22:49:31 +0200

    create task model

---
+++ lib/features/tasks/data/task_model.dart
@@ -0,0 +1,13 @@
+class TaskModel {
+  final String title;
+  final String? description;
+  final String date;
+  final String time;
+
+  TaskModel({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.time,
+  });
+}
